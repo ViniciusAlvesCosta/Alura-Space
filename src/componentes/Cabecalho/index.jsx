8f795873df2d56f352ad2a57cd49d4ae9035513c
Vinicius Costa Sato <v.alvescosta26@gmail.com>
2023-01-13 10:27:10 -0300

    cabeçalho

--- src/componentes/Cabecalho/index.jsx
+++ src/componentes/Cabecalho/index.jsx
@@ -1,13 +1,14 @@
 import logo from './logo.png'
 import search from './search.png'
+import styles from './cabecalho.module.scss'
 
 
 export default function Cabecalho() {
     return (
-        <header>
+        <header className={styles.cabecalho}>
             <img src={logo} alt="Logo do Alura Space"></img>
-            <div>
-                <input type="text" placeholder="O que você procura?"></input>
+            <div className={styles.cabecalho__container}>
+                <input className={styles.cabecalho__input} type="text" placeholder="O que você procura?"></input>
                 <img src={search} alt="Ícone de lupa"></img>
             </div>
         </header>
